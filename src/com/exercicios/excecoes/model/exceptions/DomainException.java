34a5f55ab1d475f19c871ef52c0bc8895942e54b
Ingrid Emanuelle Sousa Dias <ingrid.engdesign@outlook.com>
2022-05-04 16:01:59 -0300

    seção de exceções - solução boa - tratamento de exceções

---
+++ src/com/exercicios/excecoes/model/exceptions/DomainException.java
@@ -0,0 +1,10 @@
+package com.exercicios.excecoes.model.exceptions;
+
+public class DomainException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public DomainException(String msg) {
+		super(msg);
+	}
+	
+}
